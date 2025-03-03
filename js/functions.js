42494378761c74f9342f007d2ed20652e9374199
aniket-web-13 <aniketweb13@gmail.com>
2025-03-03 17:34:43 +0530

    head start

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,13 @@ function click() {
    return console.log("helllo");
 
 }
+
+let navNesParent = document.getElementsByClassName("nav-nes-parent");console.log(navNesParent);
+let navNesChild = document.getElementsByClassName("nav-nes-link");console.log(navNesChild);
+
+navNesParent[0].addEventListener("mouseover", ()=>{
+   navNesChild[0].style.display= "block"
+})
+navNesParent[0].addEventListener("mouseleave", ()=>{
+   navNesChild[0].style.display= "none"
+})
