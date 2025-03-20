ca9ec7d282740f338265e40ad50d25cca12ea4ef
aniket-web-13 <aniketweb13@gmail.com>
2025-03-20 16:35:06 +0530

    about page

---
+++ js/about.js
@@ -0,0 +1,9 @@
+
+
+function titleChange() {
+    let titleBox = document.getElementById("titleBox");console.log(titleBox);
+    let h1 = document.createElement("h1");console.log(h1);
+    
+}
+
+titleChange()
